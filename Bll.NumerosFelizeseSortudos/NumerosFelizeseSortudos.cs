e885a0c409f9c9b6fb22b2bcb6606423323b6563
saulopetri <47682809+saulopetri@users.noreply.github.com>
2019-06-04 19:34:39 -0300

    v 0.1

---
+++ Bll.NumerosFelizeseSortudos/NumerosFelizeseSortudos.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bll.NumerosFelizeseSortudos
+{
+    public class NumerosFelizeseSortudos
+    {
+        public Boolean isFeliz(Int32 numero)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                List<int> listOfInts = new List<int>();
+                while (numero > 0)
+                {
+                    listOfInts.Add(numero % 10);
+                    numero = numero / 10;
+                }
+
+                Int32 soma = listOfInts.Select(n => Convert.ToInt32(Math.Pow(n, 2))).Sum();
+
+                if (soma == 1)
+                    return true;
+                else
+                    numero = soma;
+            }
+
+            return false;
+        }
+
+        public bool isSortudo(int num)
+        {
+            //Gero a lista de números
+            List<int> Numeros = new List<int>();
+            for (int i = 1; i <= num; i++)
+            {
+                Numeros.Add(i);
+            }
+
+            //Retiro multiplos de 2
+            Numeros.RemoveAll(x => (x % 2 == 0));
+
+            //Vou removendo os múltiplos de cada posição da lista
+            for (int i = 1; i < Numeros.Count; i++)
+            {
+                long multiplode = Numeros[i];
+
+                int elevado = 1;
+
+                do
+                {
+                    int posicao = Convert.ToInt32(Math.Pow(multiplode, elevado));
+                    if (posicao < Numeros.Count)
+                    {
+                        Numeros.RemoveAt(posicao - 1);
+                        elevado++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+
+                } while (true);
+            }
+
+            return Numeros.Contains(num);
+        }
+    }
+}
---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,35 @@
+﻿using Bll.NumerosFelizeseSortudos;
+using System;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                NumerosFelizeseSortudos Bll = new NumerosFelizeseSortudos();
+
+                Console.Write("Digite um número inteiro:");
+
+                Int32 num;
+                bool isNum = Int32.TryParse(Console.ReadLine(), out num);
+
+                if (isNum)
+                {
+                    Boolean isFeliz = Bll.isFeliz(num);
+                    Boolean isSortudo = Bll.isSortudo(num);
+
+                    String feliz = isFeliz ? "Feliz" : "Não-Feliz";
+                    String sortudo = isSortudo ? "Sortudo" : "Não-Sortudo";
+
+                    Console.WriteLine($"{num} - Número {sortudo} e {feliz}");
+                    Console.WriteLine();
+                }
+                else
+                    Console.WriteLine("OPS! Número inválido...");
+            } while (true);
+        }
+    }
+}
